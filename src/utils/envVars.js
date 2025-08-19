9b66b137f2abf6601f94782a05888f016f277fc8
a-mancodesss <079bct011.aman@pcampus.edu.np>
2025-08-19 15:26:44 +0545

    switched to coolify backend hosting

--- src/utils/envVars.js
+++ src/utils/envVars.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_PROD
-  ? 'https://ims-be-4rqy.onrender.com'
+  ? 'https://imsbe.itclub.asmitphuyal.com.np'
   : '/api'; // local dev uses proxy
 
 export { API_BASE_URL };
