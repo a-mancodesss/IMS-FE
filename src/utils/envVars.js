99499df05f53ab912ad37b36a140834877fb6c19
a-mancodesss <079bct011.aman@pcampus.edu.np>
2025-08-19 15:32:10 +0545

    no ssl on be

--- src/utils/envVars.js
+++ src/utils/envVars.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_PROD
-  ? 'https://imsbe.itclub.asmitphuyal.com.np'
+  ? 'http://imsbe.itclub.asmitphuyal.com.np'
   : '/api'; // local dev uses proxy
 
 export { API_BASE_URL };
